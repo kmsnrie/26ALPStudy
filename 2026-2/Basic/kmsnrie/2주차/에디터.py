7dbfee6dd907811a46fa060839e942cbe41a14bb
kmsnrie <kmsnri@naver.com>
2026-09-21 00:58:12 +0900

    Create 에디터.py
    s

---
+++ 2026-2/Basic/kmsnrie/2주차/에디터.py
@@ -0,0 +1,21 @@
+left = list(input())
+right = []
+
+m = int(input())
+
+for _ in range(m):
+    command = input().split()
+    
+    if command[0] == 'L':
+        if left:
+            right.append(left.pop())
+    elif command[0] == 'D':
+        if right:
+            left.append(right.pop())
+    elif command[0] == 'B':
+        if left:
+            left.pop()
+    elif command[0] == 'P':
+        left.append(command[1])
+
+print(''.join(left + right[::-1]))
