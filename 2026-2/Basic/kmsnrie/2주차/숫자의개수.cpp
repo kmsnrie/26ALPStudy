8a2e30af5193a517f068851b9b201c7a28637a1e
kmsnrie <kmsnri@naver.com>
2026-09-21 00:24:25 +0900

    Add digit count program for product of three numbers
    s

---
+++ 2026-2/Basic/kmsnrie/2주차/숫자의개수.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int result = A * B * C;
+
+    string res_string = to_string(result);
+
+    int count[10] = {0};
+
+    for (int i = 0; i < res_string.length(); i++) {
+        int num = res_string[i] - '0';
+        count[num]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << count[i] << "\n";
+    }
+
+    return 0;
+}
